a56d4ef95f5221ec3bfda5a860a7a5cf5f65f260
Nimit_Shah <nimitshah099@gmail.com>
2025-04-05 20:06:09 +0530

    Repository created

---
+++ src/test/java/com/example/Portfolio_Website/PortfolioWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Portfolio_Website;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioWebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
